7a22e243a7c747207e07ee00b5e2c6b56a14b2ea
jiangpeipei <jiangpeipei@qutoutiao.net>
2020-05-04 23:17:48 +0800

    fix bug

--- src/main/java/com/anneng/net/api/ApiController.java
+++ src/main/java/com/anneng/net/api/ApiController.java
@@ -7,15 +7,19 @@ import com.anneng.net.model.bean.Customer;
 import com.anneng.net.model.bean.Excel;
 import com.anneng.net.model.bean.Orders;
 import com.anneng.net.model.vo.AggParams;
+import com.anneng.net.model.vo.DownLoadData;
 import com.anneng.net.model.vo.NeedDeleteOrders;
 import com.anneng.net.model.vo.OrdersParams;
 import com.anneng.net.service.AccountService;
 import com.anneng.net.util.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.List;
 
 import static com.anneng.net.util.ResponseUtil.success;
@@ -23,6 +27,7 @@ import static java.util.Objects.requireNonNull;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class ApiController {
 
     @Autowired
@@ -96,6 +101,18 @@ public class ApiController {
     @PostMapping(value = "/get/aggs/page")
     public ApiResponse getAggPage(@RequestBody AggParams params) throws IOException {
         return success(accountService.findAggPage(params));
+    }
+
+    @GetMapping("/download")
+    public void download(HttpServletResponse response, @RequestBody DownLoadData downLoadData) throws IOException {
+        // 这里注意 有同学反应使用swagger 会导致各种问题，请直接用浏览器或者用postman
+        response.setContentType("application/vnd.ms-excel");
+        response.setCharacterEncoding("utf-8");
+        // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+        String fileName = URLEncoder.encode("测试", "UTF-8");
+        response.setHeader("Content-disposition", "attachment;filename=" + fileName + ".xlsx");
+//        EasyExcel.write(response.getOutputStream(), DownloadData.class).sheet("模板").doWrite(data());
+        downLoadData.getOrders().stream().forEach(e -> log.info(e));
     }
 
 }
---
+++ src/main/java/com/anneng/net/model/vo/DownLoadData.java
@@ -0,0 +1,10 @@
+package com.anneng.net.model.vo;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class DownLoadData {
+    private List<String> orders;
+}
