d46a03f7272c611fbde7abd261e5e75e1098a50a
jiangpeipei <jiangpeipei@qutoutiao.net>
2020-05-19 18:14:08 +0800

    fix bug

--- script/tableSchema.sql
+++ script/tableSchema.sql
@@ -1,12 +1,12 @@
-use account;
-CREATE TABLE `test` (
-    `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
-    `name` varchar(64)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-/var/local/apache-maven-3.5.4/bin/mvn clean package -Dmaven.test.skip=true
-
-ps aux | grep account | grep -v grep | awk '{print $2}' | xargs kill
-#启动远程调试功能 端口号5005
-    sleep 2
-nohup java -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5009 ./target/account-1.0-SNAPSHOT.jar &
+# use account;
+# CREATE TABLE `test` (
+#     `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+#     `name` varchar(64)
+# ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+#
+# /var/local/apache-maven-3.5.4/bin/mvn clean package -Dmaven.test.skip=true
+#
+# ps aux | grep account | grep -v grep | awk '{print $2}' | xargs kill
+# #启动远程调试功能 端口号5005
+#     sleep 2
+# nohup java -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5009 ./target/account-1.0-SNAPSHOT.jar &
